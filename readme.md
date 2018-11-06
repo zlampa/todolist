3167dd60291eea1ce7acb96565cdf5dc102745b0
Zlampa <nadja.seniv@visahq.ua>
2018-11-06 17:47:24 +0200

    small_changes

--- readme.md
+++ readme.md
@@ -1,6 +1,3 @@
-# Code quality
+# ToDoList
 
-* [Jest](https://jestjs.io) - Zero configuration testing platform
-* [ESLint](https://eslint.org/) - The pluggable linting utility for JavaScript and JSX
-* [Stylelint](https://stylelint.io/) - The Modern linter that helps avoid errors and enforce conventions in styles.
-* [EditorConfig](https://editorconfig.org/) - EditorConfig helps developers define and maintain consistent coding styles between different editors and IDEs
+It is a To do list web site. The main idea is to practice web programming.
--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 const collapsible = document.getElementsByClassName('main__left__bar');
 
-collapsible[0].addEventListener("click", function() {
-    this.classList.toggle("active");
-    const content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-    } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-    }
-})
+collapsible[0].addEventListener('click', function () {
+  this.classList.toggle('active');
+  const content = this.nextElementSibling;
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null;
+  } else {
+    content.style.maxHeight = content.scrollHeight + 'px';
+  }
+});
 
 const menuBar = document.getElementById('menu_bar');
 
-menuBar.addEventListener("click", function() {
-    const hiddenMenu = document.getElementsByClassName("main__left");
-    hiddenMenu[0].classList.toggle("showed");
+menuBar.addEventListener('click', function () {
+  const hiddenMenu = document.getElementsByClassName('main__left');
+  hiddenMenu[0].classList.toggle('showed');
 });
